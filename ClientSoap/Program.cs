3f695bb6095968b15597884a6d4d128b8e3249f6
Dhamar Martínez <39349434+dhamarmj@users.noreply.github.com>
2019-12-14 13:57:17 -0400

    Subiendo

--- ClientSoap/Program.cs
+++ ClientSoap/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClientSoap.ServiceReference1;
 
 namespace ClientSoap
 {
@@ -10,6 +11,19 @@ namespace ClientSoap
     {
         static void Main(string[] args)
         {
+            UrlWebServiceClient client = new UrlWebServiceClient();
+            Console.WriteLine("URLS GENERADOS PARA EL USUARIO 1");
+            ShowurlsAsync(client);
+            Console.ReadKey();
+        }
+
+        private static void ShowurlsAsync(UrlWebServiceClient WebServiceClient)
+        {
+            var s = WebServiceClient.getUrl(1);
+            foreach (var item in s)
+            {
+                Console.WriteLine(item.url + "--> " + item.redirect);
+            }
         }
     }
 }
